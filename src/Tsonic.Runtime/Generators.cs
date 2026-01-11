9ce79636cde15a59400d1ab6d3ba4d0f6de44fff
Jeswin Kumar <jeswinpk@agilehead.com>
2026-01-11 16:49:23 +0530

    feat: add CLR-cased IteratorResult members

--- src/Tsonic.Runtime/Generators.cs
+++ src/Tsonic.Runtime/Generators.cs
@@ -5,11 +5,16 @@ namespace Tsonic.Runtime;
 /// Used by bidirectional generators (Generator&lt;TYield, TReturn, TNext&gt;).
 /// </summary>
 /// <typeparam name="T">The type of the yielded or returned value</typeparam>
-/// <param name="value">The yielded value (when done=false) or default (when done=true)</param>
-/// <param name="done">True when the generator has completed, false otherwise</param>
+/// <param name="Value">The yielded value (when done=false) or default (when done=true)</param>
+/// <param name="Done">True when the generator has completed, false otherwise</param>
 /// <remarks>
 /// Note: In JavaScript, IteratorResult.value is TYield | TReturn on completion.
 /// C# cannot represent this union, so when done=true, value is default(T).
 /// Use the generator's returnValue property to access the TReturn value after completion.
 /// </remarks>
-public readonly record struct IteratorResult<T>(T value, bool done);
+public readonly record struct IteratorResult<T>(T Value, bool Done)
+{
+    // Keep lowercase aliases for JS/TS-style `result.value` and `result.done`.
+    public T value => Value;
+    public bool done => Done;
+}
